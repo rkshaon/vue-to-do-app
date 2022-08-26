c01233be3e7ca3cb3a8ccf86d8b290b730bd3161
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-08-27 02:11:45 +0600

    view done

--- src/components/ToDoApp.vue
+++ src/components/ToDoApp.vue
@@ -1,9 +1,39 @@
 <template>
-  <!-- <div class="hello">
-    <h1>{{ msg }}</h1>    
-  </div> -->
-  <div>
-    Hello World
+  <div class="container">
+    <!-- title -->
+    <h2 class="text-center mt-5">To Do App with Vue</h2>
+    <!-- input -->
+    <div class="d-flex">
+      <input type="text" placeholder="Enter a to do task..." class="form-control">
+      <button class="btn btn-warning rounded-0">Sumibt</button>
+    </div>
+    <!-- task list table -->
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col">Status</th>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">#</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>{{task.name}}</td>
+          <td>{{task.status}}</td>
+          <td>
+            <div class="text-center">
+              <span class="fa fa-pen"></span>
+            </div>
+          </td>
+          <td>
+            <div class="text-center">
+              <span class="fa fa-trash"></span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -12,6 +42,28 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          name: 'Hello',
+          status: 'to-do'
+        },
+        {
+          name: 'buy something for Bhabi',
+          status: 'to-do'
+        },
+        {
+          name: 'git-post-new-repo',
+          status: 'to-do',
+        },
+        {
+          name: 'post-on-linked-in',
+          status: 'to-do'
+        }
+      ]
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 createApp(App).mount('#app')
